726317c4477a83acf8e147304c14e48ed5d5db55
wagner <wagncarv@outlook.com>
2023-07-20 10:21:33 -0300

    constructor

--- modulo_2/classes.js
+++ modulo_2/classes.js
@@ -1,5 +1,14 @@
 class Human {
     hungry = true
+    name
+    age
+    sex
+
+    constructor(props) {
+        this.name = props.name
+        this.age = props.age;
+        this.sex = props.sex;
+    }
 
     eat () {
         this.hungry = false
@@ -7,5 +16,5 @@ class Human {
 
 }
 
-const person = new Human()
+const person = new Human({name:'Acme', age: 33, sex: 'male'})
 console.log(person)
