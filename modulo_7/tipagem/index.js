68bfd353bac3bbc8c1053a7f7fb3e08fd1a2508d
wagner <wagncarv@outlook.com>
2023-07-21 18:29:50 -0300

    Tipando variáveis

---
+++ modulo_7/tipagem/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = 'Acme';
+const age = 33;
+const is_alive = true;
+const contacts = ['acme@mail.com', 999999999];
---
+++ modulo_7/tipagem/index.ts
@@ -0,0 +1,4 @@
+const personName: string = 'Acme'
+const age: number = 33
+const is_alive: boolean = true
+const contacts:(string | number)[] = ['acme@mail.com', 999999999]
